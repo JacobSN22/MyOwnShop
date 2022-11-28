a7d82b6ba838ea54ba016332c9a16038f3e5e3e0
Jacob <jacobsimoninielsen@gmail.com>
2022-11-28 13:52:01 +0100

    Mandag

---
+++ Routes/song-router.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/song', (req, res) => {
+    console.log('Kalder /song med GET');
+})
+
+router.get('/song/:id([0-9]*)', (req, res) => {
+    console.log('Detaljer: Kalder /song med GET')
+}) 
+
+router.post('/song', (req, res) => {
+    console.log('Opret: Kalder /song med POST')
+})
+
+router.put('/song', (req, res) => {
+    console.log('Opdater: Kalder /song med PUT')
+})
+
+router.delete('/song', (req, res) => {
+    console.log('Slet: Kalder /song med DELETE')
+})
+
+export { router }
---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import { router as SongRouter } from './Routes/song-router.js'
+
+const app = express()
+const port = 3000
+
+// App settings to provide access to request body data
+app.use(express.urlencoded({ extended: true }))
+
+// app.get('/', (req, res) => {
+//     res.send('Frontpage')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('About us')
+// })
+
+// app.post('/song', (req, res) => {
+//     console.log(req.body);
+// })
+
+// app.get('/Work', (req, res) => {
+//     res.send('Our work');
+// })
+
+// app.get('/FindUs', (req, res) => {
+//     res.send('Find us');
+// })
+
+// app.get('/Products', (req, res) => {
+//     res.send('Our products');
+// })
+
+// app.get('/ProductDetails', (req, res) => {
+//     res.send('Product details');
+// })
+
+// app.get('/Contact', (req, res) => {
+//     res.send('Contact us');
+// })
+
+app.use(SongRouter)
+
+app.listen(port, () => {
+    console.log(`Webserver running on http://localhost:${port}`);
+});
